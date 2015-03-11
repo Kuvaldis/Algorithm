32b9f6af9af53fdfea11bd9e86426eaaf96e926b
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-03-11 22:23:43 +0300

    more generic abstract backtrack

--- src/main/java/kuvaldis/algorithm/backtracking/AbstractBacktrack.java
+++ src/main/java/kuvaldis/algorithm/backtracking/AbstractBacktrack.java
@@ -7,22 +7,22 @@ import java.util.List;
  * Based on DFS, as it takes less memory. We don't care about execution stack depth, consider it infinite.
  *
  */
-public abstract class AbstractIntegerBacktrack<R, D> implements Backtrack<R, D> {
+public abstract class AbstractBacktrack<E, R, D> implements Backtrack<R, D> {
 
     @Override
     public R backtrack(D input) {
-        final List<Integer> solutionsList = new ArrayList<>();
+        final List<E> solutionsList = new ArrayList<>();
         doBacktrack(solutionsList, 0, input);
         return constructResult(input);
     }
 
-    protected boolean doBacktrack(final List<Integer> solutionsList, final int start, final D input) {
+    protected boolean doBacktrack(final List<E> solutionsList, final int start, final D input) {
         int k = start;
         if (isSolution(solutionsList, k, input)) {
             processSolution(solutionsList, k, input);
         } else {
-            final List<Integer> candidates = constructCandidates(solutionsList, ++k, input);
-            for (Integer candidate : candidates) {
+            final List<E> candidates = constructCandidates(solutionsList, ++k, input);
+            for (E candidate : candidates) {
                 addToSolutionsList(solutionsList, k, candidate);
                 if (makeMove(solutionsList, k, input)) {
                     return true;
@@ -38,7 +38,7 @@ public abstract class AbstractIntegerBacktrack<R, D> implements Backtrack<R, D> 
         return false;
     }
 
-    private void addToSolutionsList(final List<Integer> solutionsList, final int addPosition, final Integer candidate) {
+    private void addToSolutionsList(final List<E> solutionsList, final int addPosition, final E candidate) {
         if (solutionsList.size() - 1 < addPosition) {
             solutionsList.add(candidate);
         } else {
@@ -46,19 +46,19 @@ public abstract class AbstractIntegerBacktrack<R, D> implements Backtrack<R, D> 
         }
     }
 
-    protected boolean makeMove(List<Integer> solutionsList, int k, D input) {
+    protected boolean makeMove(List<E> solutionsList, int k, D input) {
         return false;
     }
 
-    protected boolean unmakeMove(List<Integer> solutionsList, int k, D input) {
+    protected boolean unmakeMove(List<E> solutionsList, int k, D input) {
         return false;
     }
 
     protected abstract R constructResult(D input);
 
-    protected abstract boolean isSolution(List<Integer> solutionsList, int k, D input);
+    protected abstract boolean isSolution(List<E> solutionsList, int k, D input);
 
-    protected abstract void processSolution(List<Integer> solutionsList, int k, D input);
+    protected abstract void processSolution(List<E> solutionsList, int k, D input);
 
-    protected abstract List<Integer> constructCandidates(List<Integer> solutionsList, int k, D input);
+    protected abstract List<E> constructCandidates(List<E> solutionsList, int k, D input);
 }
