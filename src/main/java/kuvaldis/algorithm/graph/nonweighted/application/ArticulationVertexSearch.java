274443dadb096469b3345303026636758eb9f9a1
kuvaldis <kuvaldis1988@gmail.com>
2015-12-11 03:43:52 +0300

    articulation vertex search some fixes

--- src/main/java/kuvaldis/algorithm/graph/nonweighted/application/ArticulationVertexSearch.java
+++ src/main/java/kuvaldis/algorithm/graph/nonweighted/application/ArticulationVertexSearch.java
@@ -6,6 +6,8 @@ import kuvaldis.algorithm.graph.nonweighted.domain.Vertex;
 
 import java.util.*;
 
+// looks for articulation tree in connected graph
+// works only for undirected graphs
 public class ArticulationVertexSearch extends AbstractDepthFirstSearch<Map<ArticulationVertexSearch.ArticulationVertexType, Set<Vertex>>> {
 
     private final HashMap<ArticulationVertexType, Set<Vertex>> result = new HashMap<>();
@@ -14,8 +16,10 @@ public class ArticulationVertexSearch extends AbstractDepthFirstSearch<Map<Artic
         ROOT, PARENT, BRIDGE
     }
 
+    // the oldest (lesser entry time) ancestor of the key vertex which is accessible for it
     private final Map<Vertex, Vertex> reachableAncestor = new HashMap<>();
 
+    // how many vertices the key vertex is the parent of
     private final Map<Vertex, Integer> treeOutDegree = new HashMap<>();
 
     public ArticulationVertexSearch(Graph graph) {
@@ -30,11 +34,16 @@ public class ArticulationVertexSearch extends AbstractDepthFirstSearch<Map<Artic
 
     @Override
     protected boolean processEdge(Vertex v, Vertex y) {
+        if (y.isProcessed()) {
+            return true;
+        }
         final EdgeClass edgeClass = edgeClassification(v, y);
         if (EdgeClass.TREE.equals(edgeClass)) {
             treeOutDegree.put(v, treeOutDegree.getOrDefault(v, 0) + 1);
         }
-        if (EdgeClass.BACK.equals(edgeClass)) {
+        // we reached here a reachable ancestor
+        if (EdgeClass.BACK.equals(edgeClass) && y != v.getParent()) {
+            // check if it's older then current
             if (entryTime(y) < entryTime(reachableAncestor.get(v))) {
                 reachableAncestor.put(v, y);
             }
@@ -45,22 +54,30 @@ public class ArticulationVertexSearch extends AbstractDepthFirstSearch<Map<Artic
     @Override
     protected boolean postProcessVertex(Vertex vertex) {
         if (vertex.getParent() == null) { // root
-            if (treeOutDegree.get(vertex) > 1) {
+            // there is at least two tree children, so it is an articulation vertex with type ROOT
+            if (treeOutDegree.getOrDefault(vertex, 0) > 1) {
                 result.put(ArticulationVertexType.ROOT, Collections.singleton(vertex));
             }
             return true;
         }
+        // oldest reachable ancestor of the vertex is it's parent who is not the root.
+        // definitely a PARENT articulation tree
         if (reachableAncestor.get(vertex).equals(vertex.getParent()) && vertex.getParent().getParent() != null) {
             result.putIfAbsent(ArticulationVertexType.PARENT, new HashSet<>());
             result.get(ArticulationVertexType.PARENT).add(vertex.getParent());
         }
+        // vertex doesn't reach it's ancestors
         if (reachableAncestor.get(vertex).equals(vertex)) {
             result.putIfAbsent(ArticulationVertexType.BRIDGE, new HashSet<>());
-            result.get(ArticulationVertexType.BRIDGE).add(vertex.getParent());
+            // root may be only a root articulation vertex
+            if (vertex.getParent().getParent() != null) {
+                result.get(ArticulationVertexType.BRIDGE).add(vertex.getParent());
+            }
             if (treeOutDegree.getOrDefault(vertex, 0) > 0) { // not tree leaf
                 result.get(ArticulationVertexType.BRIDGE).add(vertex);
             }
         }
+        // propagate reachable ancestor to the parent if the ancestor is older
         if (entryTime(reachableAncestor.get(vertex)) < entryTime(reachableAncestor.get(vertex.getParent()))) {
             reachableAncestor.put(vertex.getParent(), reachableAncestor.get(vertex));
         }
--- src/test/java/kuvaldis/algorithm/graph/nonweighted/application/ArticulationVertexSearchTest.java
+++ src/test/java/kuvaldis/algorithm/graph/nonweighted/application/ArticulationVertexSearchTest.java
@@ -16,15 +16,21 @@ public class ArticulationVertexSearchTest {
         Graph graph = GraphUtils.fromResource("articulation_vertex_graph_1.txt");
         Map<ArticulationVertexSearch.ArticulationVertexType, Set<Vertex>> result =
                 new ArticulationVertexSearch(graph).search().result();
-        assertEquals(Arrays.asList(1), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.ROOT));
-        assertEquals(Arrays.asList(2), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.PARENT));
-        assertEquals(Arrays.asList(1, 2), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.BRIDGE));
+        assertEquals(Collections.singletonList(1), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.ROOT));
+        assertEquals(Collections.singletonList(2), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.PARENT));
+        assertEquals(Collections.singletonList(2), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.BRIDGE));
 
         graph = GraphUtils.fromResource("articulation_vertex_graph_2.txt");
         result = new ArticulationVertexSearch(graph).search().result();
-        assertEquals(Arrays.asList(1), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.ROOT));
+        assertEquals(Collections.singletonList(1), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.ROOT));
         assertEquals(Arrays.asList(4, 8), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.PARENT));
-        assertEquals(Arrays.asList(3, 4), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.BRIDGE));
+        assertEquals(Arrays.asList(3, 4, 11, 12), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.BRIDGE));
+
+        graph = GraphUtils.fromResource("articulation_vertex_graph_3.txt");
+        result = new ArticulationVertexSearch(graph).search().result();
+        assertEquals(Collections.singletonList(1), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.ROOT));
+        assertEquals(Collections.singletonList(3), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.PARENT));
+        assertEquals(Arrays.asList(2, 3), getNumbers(result, ArticulationVertexSearch.ArticulationVertexType.BRIDGE));
     }
 
     private List<Integer> getNumbers(final Map<ArticulationVertexSearch.ArticulationVertexType, Set<Vertex>> result,
