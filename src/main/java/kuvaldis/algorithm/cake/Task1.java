1266a081eb63f555ef06840fd00825bab6208470
kuvaldis <kuvaldis1988@gmail.com>
2015-12-23 22:24:31 +0300

    interview cake task1

--- src/main/java/kuvaldis/algorithm/cake/Task1.java
+++ src/main/java/kuvaldis/algorithm/cake/Task1.java
@@ -1,11 +1,18 @@
 package kuvaldis.algorithm.cake;
 
-public class Task1 {
-    public static void main(final String[] args) {
-        int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9};
+/*
 
-        System.out.print(new Task1().getMaxProfit(stockPricesYesterday));
-    }
+Writing coding interview questions hasn't made me rich. Maybe trading Apple stocks will.
+Suppose we could access yesterday's stock prices as an array, where:
+
+The indices are the time in minutes past trade opening time, which was 9:30am local time.
+The values are the price in dollars of Apple stock at that time.
+So if the stock cost $500 at 10:30am, stockPricesYesterday[60] = 500.
+
+Write an efficient function that takes stockPricesYesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+
+ */
+public class Task1 {
 
     public int getMaxProfit(final int[] prices) {
         return findMinMaxProfit(prices, 0, prices.length)[2];
