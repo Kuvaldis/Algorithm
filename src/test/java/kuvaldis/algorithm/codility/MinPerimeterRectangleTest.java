a7287733049463d0a0e13b3baa46dbedaba51b0b
Nikolay Fadin <kuvaldis1988@gmail.com>
2016-01-25 15:09:22 +0300

    codility MinPerimeterRectangle Find the minimal perimeter of any rectangle whose area equals N.

---
+++ src/test/java/kuvaldis/algorithm/codility/MinPerimeterRectangleTest.java
@@ -0,0 +1,13 @@
+package kuvaldis.algorithm.codility;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinPerimeterRectangleTest {
+
+    @Test
+    public void testSolution() throws Exception {
+        assertEquals(22, new MinPerimeterRectangle().solution(30));
+    }
+}
