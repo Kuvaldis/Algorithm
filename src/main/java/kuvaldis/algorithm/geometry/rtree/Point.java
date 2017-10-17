ced97f1744739a296e05b0f3d2b3650bb23038ac
Kuvaldis <kuvaldis1988@gmail.com>
2017-10-17 23:15:01 +0300

    some tests for r-tree

--- src/main/java/kuvaldis/algorithm/geometry/rtree/Point.java
+++ src/main/java/kuvaldis/algorithm/geometry/rtree/Point.java
@@ -18,4 +18,27 @@ public class Point {
     public int getY() {
         return y;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Point point = (Point) o;
+
+        if (x != point.x) return false;
+        return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s,%s)", x, y);
+    }
 }
---
+++ src/test/java/kuvaldis/algorithm/geometry/rtree/RTreeTest.java
@@ -0,0 +1,52 @@
+package kuvaldis.algorithm.geometry.rtree;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class RTreeTest {
+
+    @Test
+    public void sanityCheck() throws Exception {
+        // given
+        final RTree rTree = new RTree(4, 2);
+
+        // when
+        rTree.add(new Point(3, 4));
+        rTree.add(new Point(1, 7));
+        rTree.add(new Point(7, 1));
+        rTree.add(new Point(2, 2));
+        rTree.add(new Point(5, 7));
+        rTree.add(new Point(3, 6));
+        rTree.add(new Point(6, 3));
+        rTree.add(new Point(4, 6));
+        rTree.add(new Point(6, 5));
+        rTree.add(new Point(8, 1));
+        rTree.add(new Point(3, 5));
+        rTree.add(new Point(4, 5));
+        rTree.add(new Point(1, 1));
+        rTree.add(new Point(10, 2));
+        rTree.add(new Point(2, 3));
+        rTree.add(new Point(9, 5));
+        rTree.add(new Point(2, 8));
+
+        // then
+        final List<Point> nonEmptyResult = rTree.search(new Area(new Point(4, 3), new Point(9, 7)));
+        assertThat(nonEmptyResult, Matchers.containsInAnyOrder(
+                new Point(4, 5),
+                new Point(4, 6),
+                new Point(5, 7),
+                new Point(6, 3),
+                new Point(6, 5),
+                new Point(9, 5)
+        ));
+
+        // and
+        final List<Point> emptyResult = rTree.search(new Area(new Point(7, 7), new Point(8, 8)));
+        assertTrue(emptyResult.isEmpty());
+    }
+}
