23f1dff2455d0730b4b9234e608a2b9482ab8060
Kuvaldis <kuvaldis1988@gmail.com>
2017-12-12 22:09:45 +0300

    search substring refactoring. migrate from jbehave to simple junit parametrized test

--- src/jmh/java/kuvaldis/algorithm/string/SearchStringBenchmark.java
+++ src/jmh/java/kuvaldis/algorithm/string/SearchStringBenchmark.java
@@ -7,22 +7,22 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class SearchStringBenchmark {
 
-    private char[] randomChars(int bits) {
-        return new BigInteger(bits, ThreadLocalRandom.current()).toString(32).toCharArray();
+    private CharSequence randomChars(int bits) {
+        return new BigInteger(bits, ThreadLocalRandom.current()).toString(32);
     }
 
     @Benchmark
     public void run1to2() {
-        new SearchString().search(randomChars(1), randomChars(2));
+        new SimpleSubstringSearch().search(randomChars(1), randomChars(2));
     }
 
     @Benchmark
     public void run100to200() {
-        new SearchString().search(randomChars(100), randomChars(200));
+        new SimpleSubstringSearch().search(randomChars(100), randomChars(200));
     }
 
     @Benchmark
     public void run10000to20000() {
-        new SearchString().search(randomChars(10000), randomChars(20000));
+        new SimpleSubstringSearch().search(randomChars(10000), randomChars(20000));
     }
 }
